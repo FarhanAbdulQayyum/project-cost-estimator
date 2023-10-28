8d1bd73e57e946c359191fe0b749e45b662836b8
Farhan <livefarhan1@gmail.com.com>
2023-10-29 02:48:59 +0500

    project item styling

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -1,8 +1,20 @@
 import { Box, HStack, Image, Text } from "@chakra-ui/react"
 import projectImageIcon from "../assets/project-icon.png"
 
+const ProjectItemStyles = {
+    py: "5px",
+    border: "1px solid",
+    borderColor: "gray.900",
+    borderRadius: "5px",
+    w: "100%",
+    cursor: "pointer",
+    ':hover': {
+        bgColor: "gray.500"
+    }
+}
+
 export const ProjectItem = ({ name }: { name: string }) => (
-    <Box py="5px" border="1px solid" borderColor="gray.900" borderRadius="5px" w="100%">
+    <Box sx={ProjectItemStyles}>
         <HStack spacing="0px">
             <Box height="40px" width="40px">
                 <Image src={projectImageIcon} />
