d3dcbb5211ac7a2e96055e464a945de47b0eb5cd
Farhan <livefarhan1@gmail.com.com>
2023-10-31 01:28:57 +0500

    text change

--- src/components/ProjectResourceHeader.tsx
+++ src/components/ProjectResourceHeader.tsx
@@ -4,7 +4,7 @@ export const ProjectResourceHeader = () => {
     return (
         <SimpleGrid fontWeight="500" spacing={20} columns={4} width="60%" mb={2}>
             <Text>Name</Text>
-            <Text>Unit Price</Text>
+            <Text>Price</Text>
             <Text>Quantity</Text>
             <Text>Sub Total</Text>
         </SimpleGrid>
