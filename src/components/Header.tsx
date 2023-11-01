3f5b9b56b0755a3e8b0ab9d20572f1e6281104c8
Farhan <livefarhan1@gmail.com.com>
2023-11-01 13:43:03 +0500

    create project button removed from header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,6 @@
-import { Box, Button, HStack, Heading } from "@chakra-ui/react";
-import { data, getNewId } from "../data/data";
-import { useNavigate } from 'react-router-dom';
+import { Button, HStack, Heading } from "@chakra-ui/react";
 
 export const Header = () => {
-    const navigate = useNavigate();
-
-    const createProject = () => {
-        const newId = getNewId();
-        data.projects.push({ id: newId, name: `New Project ${newId}`, children: [] })
-        navigate('/project-details', { state: { id: newId } })
-    }
 
     return (
         <HStack justifyContent="space-between" backgroundColor="blue.900" width="100%" p="10px" >
@@ -17,7 +8,6 @@ export const Header = () => {
                 Project Cost Estimator
             </Heading>
             <HStack spacing="10px">
-                <Button onClick={createProject} colorScheme="blue">Create Project</Button>
                 <Button>Upload Project</Button>
             </HStack>
         </HStack>
